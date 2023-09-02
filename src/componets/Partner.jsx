b29e4113abd2974304488857643f062adcd05693
miidlaj <mumidlaj@gmail.com>
2023-09-02 18:58:23 +0530

    edit

--- src/componets/Partner.jsx
+++ src/componets/Partner.jsx
@@ -4,74 +4,70 @@ import React, { useEffect, useState } from "react";
 import { getAllPartnerImagees } from "../api/Partner";
 
 const Partner = () => {
-
-  const [images, setImages] = useState([])
+  const [images, setImages] = useState([]);
 
   const fetchImages = async () => {
     await getAllPartnerImagees().then((res) => {
       if (res?.status === 200) {
         setImages(res?.data?.imageData);
-      } 
+      }
     });
   };
 
   useEffect(() => {
     fetchImages();
+  }, [images]);
 
-  }, [images])
-  
-  
   return (
     <>
       <section className="partner-area">
         <div className="">
-        <div className="sec-title text-center">
+          <div className="sec-title text-center">
             <p>Our Partners</p>
-           
           </div>
           <div className="row">
-
             <div className="partner-slider">
               <div className="slide-track">
+                {images.length !== 0 &&
+                  images.map((img) => (
+                    <div className="slide">
+                      <img src={img.imageUrl} height={100} width={250} alt="" />
+                    </div>
+                  ))}
 
-                {images.length !== 0 && images.map((img) => (
-                  <div className="slide">
-                  <img
-                    src={img.imageUrl}
-                    height={100}
-                    width={250}
-                    alt=""
-                  />
-                </div>
-                ))}
-
+                {images.length !== 0 &&
+                  images.map((img) => (
+                    <div className="slide">
+                      <img src={img.imageUrl} height={100} width={250} alt="" />
+                    </div>
+                  ))}
 
                 {images.length === 0 && (
                   <>
-                  <div className="slide">
-                  <img
-                    src="assets/images/brand/brand-1.png"
-                    height={100}
-                    width={250}
-                    alt=""
-                  />
-                </div>
-                <div className="slide">
-                  <img
-                    src="assets/images/brand/brand-2.png"
-                    alt="Awesome Image"
-                    height={100}
-                    width={250}
-                  />
-                </div>
-                <div className="slide">
-                  <img
-                    src="assets/images/brand/brand-3.png"
-                    alt="Awesome Image"
-                    height={100}
-                    width={250}
-                  />
-                </div>
+                    <div className="slide">
+                      <img
+                        src="assets/images/brand/brand-1.png"
+                        height={100}
+                        width={250}
+                        alt=""
+                      />
+                    </div>
+                    <div className="slide">
+                      <img
+                        src="assets/images/brand/brand-2.png"
+                        alt="Awesome Image"
+                        height={100}
+                        width={250}
+                      />
+                    </div>
+                    <div className="slide">
+                      <img
+                        src="assets/images/brand/brand-3.png"
+                        alt="Awesome Image"
+                        height={100}
+                        width={250}
+                      />
+                    </div>
                   </>
                 )}
               </div>
